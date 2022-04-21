1d68649bf267681e6010fce5bd78c59c233e0e6e
carmar14 <carmar141414@hotmail.com>
2022-04-21 14:10:44 -0500

    Actualizacion solver parcial

--- G1/S12/parcial 2 dinamicos/parcial.m
+++ G1/S12/parcial 2 dinamicos/parcial.m
@@ -17,11 +17,11 @@ fv=1;
 N1=10;
 N2=20;
 
-i=3; %selección de caso para el if
+i=1; %selección de caso para el if
 if i==1
     %----ejemplo con SO% y un Ts en particular----
-    SO=12;
-    Ts=8;
+    SO=15;
+    Ts=15;
     %-------se despejan apropiadamente las ecuaciones del SO% y del tiempo de
     %estabilización y se encuentra zeta y omega_n para el caso------------
     u=SO/100;
@@ -34,7 +34,7 @@ if i==1
     K=(5+4*M)*wn^2
 else
     %----ejemplo con un Ts en particular----
-    Ts=50; %Ts=4tao ---- tao dominante---
+    Ts=150; %Ts=4tao ---- tao dominante---
     tao1=Ts/4;  %dará el polo dominante
     tao2= tao1/100; % dará el polo despreciable
     %variables a usar cuando se conecta en cascada los dos sistemas de
@@ -43,8 +43,10 @@ else
     a2= 1/(tao1*tao2);
     %encontrando los parametros de M y K
     M=(12/a1-5)/4
-    K=a2
+    K=a2*(5+4*M)
 end
+
+
 h=tf(Kt*R/Ra*(N2/N1), [J+Ja*(N2/N1)^2+M*R^2 fv*R^2+Da*(N2/N1)^2+Kt*Kb/Ra*(N2/N1)^2 K]);
 
 h2=tf(4,[5+4*M 12 K]);
@@ -67,6 +69,8 @@ R2=0.025;
 K2=5000;
 K3=1200;
 B=4500;
+
+
 
 %------------punto 2---------
 
